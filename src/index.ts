0eec01fa70fc43ab45fdbb6f2ca73b757baa48fc
hirakinii <hiraki@nii.ac.jp>
2026-02-23 19:13:17 +0900

    feat: phase 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export type { RorOrganization } from './types/ror-organization.js';
+export { rorOrganizationSchema, isRorOrganization } from './types/ror-organization.js';
+export { RorClient } from './client/ror-client.js';
+export type { RorClientOptions, FilterParams, FilterKey } from './client/ror-client.js';
+export { BASE_URL, HEALTH_CHECK_URL } from './constants/urls.js';
